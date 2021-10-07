fc6f5a22c8588eb8f5034d833ba8131884cb0653
jefferson1104 <jefferson1104junior@gmail.com>
2021-10-06 23:44:03 -0300

    responsividade do Header e do Sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Jefferson Soares</Text>
-        <Text color="gray.300" fontSize="small">
-          jefferson1104junior@gmail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Jefferson Soares</Text>
+          <Text color="gray.300" fontSize="small">
+            jefferson1104junior@gmail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar size="md" name="Jefferson Soares" src="https://github.com/jefferson1104.png" />
     </Flex>
   );
