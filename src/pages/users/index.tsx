2e9c33b4e07d7b956e710570ae94b7bd24bbc757
jefferson1104 <jefferson1104junior@gmail.com>
2021-10-07 14:59:58 -0300

    Configurando usuarios fakes no miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Link from "next/link";
 import { 
   Box,
@@ -25,6 +26,12 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
